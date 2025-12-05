71359c25632483b930ed149cdc802ff2fd033ff8
Alex <truonggiang190689@gmail.com>
2025-12-05 18:23:46 +0700

    Refactor package.json and pnpm-lock.yaml to streamline build and start scripts, removing unnecessary filters. Update Turbo configuration for improved task management. Add production environment configuration for dApp and enhance VSCode settings for better formatting and linting support.

--- apps/docs/src/app/layout.js
+++ apps/docs/src/app/layout.js
@@ -4,6 +4,7 @@ import { Footer, Layout, Navbar } from "nextra-theme-docs";
 import "nextra-theme-docs/style.css";
 import Image from "next/image";
 import { useThemeConfig } from "nextra-theme-docs";
+import "./globals.css";
 
 export const metadata = {
 	// Define your metadata here
@@ -14,7 +15,27 @@ export const metadata = {
 const navbar = (
 	<Navbar
 		logo={
-			<Image src="/blacktext.svg" alt="Sora Vault" width={100} height={100} />
+			<div className="h-full">
+				<p className="sr-only">Axyl</p>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 3207 485"
+					fill="currentColor"
+					height={12}
+				>
+					<title>blacktext</title>
+					<line
+						x1="362.28"
+						y1="151"
+						x2="362"
+						y2="151.16"
+						stroke="currentColor"
+					/>
+					<polygon points="2446 0 2446 87.14 2380.42 125 2335.39 151 2118.88 276 2075 301.33 2018.42 334 1855.8 427.89 1756.88 485 1455 485 1455 334 1716.54 334 1817.03 276 1592.64 276 1455 196.56 1455 151 1373.92 151 795.41 485 362 485 362 334 754.94 334 855.4 276 930.75 232.5 789.62 151 362 151 362 151.16 151 272.98 151 485 0 485 0 185.8 60.28 151 105.31 125 151 98.62 321.82 0 830.32 0 1046.89 125 1081.93 145.22 1116.95 125 1223.97 63.21 1333.46 0 1606 0 1606 151 2033.6 151 2075 127.11 2078.65 125 2295.22 0 2446 0" />
+					<polygon points="1397.25 327.22 1397.25 485 1368 485 1140.84 353.82 1175.18 334 1275.64 276 1292.07 266.51 1308.51 276 1397.25 327.22" />
+					<polygon points="3207 217.14 3207 485 2075 485 2075 368.31 2134.42 334 2234.88 276 2446 154.11 2451.39 151 2496.42 125 2671.08 24.16 2712.93 0 2876 0 2876 81.17 2822.91 111.82 2800.09 125 2755.06 151 2538.55 276 2446 329.43 2446 334 3056 334 3056 305.29 3005.28 276 2920 226.77 2920 151 3092.44 151 3207 217.14" />
+				</svg>
+			</div>
 		}
 		// ... Your additional navbar options
 	/>
